5702ae6fdd31ed61edbe0a51254e2bd296787e2f
Falk Hoppe <falkhoppe81@gmail.com>
2015-07-14 18:06:34 +0200

    and setup goes on

--- gulpfile.js
+++ gulpfile.js
@@ -9,10 +9,10 @@
       stylus = require('gulp-stylus'),
       jade = require('gulp-jade');
 
-  gulp.task('styleguide', function() {
-    gulp.src('./styleguide/**/*.jade')
+  gulp.task('style_guide', function() {
+    gulp.src('./style_guide/**/*.jade')
       .pipe(jade({pretty: true, compileDebug: false, debug: false}).on('error', gutil.log))
-      .pipe(gulp.dest('./dist/styleguide'));
+      .pipe(gulp.dest('./dist/style_guide'));
   });
 
   gulp.task('styles', function () {
@@ -24,9 +24,9 @@
       .pipe(gulp.dest('./dist'));
   });
 
-  gulp.task('watch', ['styles', 'styleguide'], function() {
+  gulp.task('watch', ['styles', 'style_guide'], function() {
     gulp.watch('./styles/**/*.style', ['styles']);
-    gulp.watch('./styleguide/**/*', ['styleguide']);
+    gulp.watch('./styleguide/**/*', ['style_guide']);
   });
 
   gulp.task('default', ['watch']);
